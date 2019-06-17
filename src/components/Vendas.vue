c0a51a0113f81ccec00791655e08325fa25dac57
Leandro S Araujo <leandro.souara.web@gmail.com>
2019-06-17 18:42:16 -0400

    vendas component

--- src/components/Vendas.vue
+++ src/components/Vendas.vue
@@ -3,21 +3,25 @@
     <h3>
       <i class="small material-icons">add_circle_outline</i>
       Nova Venda
-      <router-link to="/vendas/lista" class="btn">Listar Vendas</router-link>
     </h3>
     <div class="row">
       <form class="col s12">
         <div class="row">
-          <div class="input-field col m12 s12">
-            <input id="nome" type="text" class="validate" v-model="vendaDados.nome">
-            <label v-bind:class="{ active: labelIsActive }" for="nome">Nome do Produto</label>
+          <div class="input-field col m9 s12">
+            <input v-model="query" id="nome" type="text" class="validate">
+            <label for="nome">Nome do Produto</label>
+          </div>
+          <div class="input-field col m3 s12">
+            <a @click.prevent="buscarProduto" class="waves-effect waves-light btn">Buscar</a>
           </div>
         </div>
-        <div class="row">
+        <!-- <div class="row">
           <a @click.prevent="adicionarOuCriarProduto" class="waves-effect waves-light btn">Salvar</a>
-        </div>
+        </div>-->
       </form>
     </div>
+
+    <div v-bind:class="{ active: boxIsActive }" id="produtos-search">teste</div>
   </div>
 </template>
 
@@ -28,12 +32,38 @@ export default {
   name: 'VendasNova',
   data () {
     return {
+      query: '',
       vendaDados: {
         produto_id: '',
         nome: '',
         preco: '',
         estoque: '',
         preco_custo: ''
+      },
+      boxIsActive: false
+    }
+  },
+  create () {
+    db.collection('Produtos')
+      .get()
+      .then(querySnapshot => {
+        const produtos = []
+        querySnapshot.forEach(doc => {
+          produtos.push(doc.data())
+        })
+        this.produtos = produtos
+      })
+  },
+  computed: {
+    filtrarProdutos () {
+      return this.produtos.filter(produto => {
+        return '';
+      })
+    }
+  },
+  methods: {
+    buscarProduto () {
+      if (this.query !== '') {
       }
     }
   }
